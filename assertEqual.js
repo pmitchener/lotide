5f7c4b663a7ef644aac5837d38078ab2466d8a6e
Patrick Mitchener <pmitchener@rogers.com>
2020-07-21 00:46:15 +0000

    Could not get emojis to work

---
+++ assertEqual.js
@@ -0,0 +1,10 @@
+//could not get emojis to work.
+const assertEqual = function(actual, expected) {
+  const passEmoji =  String.fromCodePoint(0x1F600);
+  const failEmoji = '\u1F923';
+  actual === expected ? console.log(` Assertion Passed: ${actual} === ${expected}`) : console.log(`${failEmoji} Assertion Failed: ${actual} !== ${expected}`);
+};
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(3+4, 6);
+assertEqual(10+2 , 12);
