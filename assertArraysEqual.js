f08b4a7f317e915451fb6b432d6e7f4e5d9e4cba
Patrick Mitchener <pmitchener@rogers.com>
2020-07-22 02:07:51 +0000

    Adding changes

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,8 +1,7 @@
-const assertArraysEqual = function(list1, list2, expected) {
+const assertArraysEqual = function(list1, list2) {
   const passEmoji =  String.fromCodePoint(0x2705);
   const failEmoji = '\u1F923';
-  let actual = eqArrays(list1, list2);
-  actual === expected ? console.log(`${passEmoji} Assertion Passed: ${actual} === ${expected}`) : console.log(`${failEmoji} Assertion Failed: ${actual} !== ${expected}`);
+  eqArrays(list1, list2) ? console.log(`${passEmoji} Assertion Passed`) : console.log(`${failEmoji} Assertion Failed`);
 };
 
 const eqArrays = (list1, list2) => {
@@ -16,8 +15,8 @@ const eqArrays = (list1, list2) => {
   }
   return true;
 };
-assertArraysEqual([1, 2, 3], [1, 2, 3], true); // => should PASS
-assertArraysEqual([1, 2, 3], [3, 2, 1], false); // => false
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // => should PASS
+assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
 
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"], true); // => true
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3], false); // => false
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => false
---
+++ without.js
@@ -0,0 +1,38 @@
+const assertArraysEqual = function(list1, list2) {
+  const passEmoji =  String.fromCodePoint(0x2705);
+  const failEmoji = '\u1F923';
+  eqArrays(list1, list2) ? console.log(`${passEmoji} Assertion Passed`) : console.log(`${failEmoji} Assertion Failed`);
+};
+
+const eqArrays = (list1, list2) => {
+  if(list1.length !== list2.length) {
+    return false;
+  }
+  for (let i = 0; i < list1.length; i++) {
+    if(list1[i] !== list2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const without = (source, itemstoRemove) => {
+  let newArray = [];
+  for(let x = 0; x < source.length; x++) {
+    let itemPassed = true;
+    for(let y = 0; y < itemstoRemove.length; y++) {
+      if(source[x] === itemstoRemove[y]) {
+        itemPassed = false;
+        break;
+      }
+    }
+    if (itemPassed) {
+      newArray.push(source[x]);
+    }
+  }
+  return newArray;
+};
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
