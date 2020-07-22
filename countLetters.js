6718441913b6ccc057cf2045caf21960a1644606
Patrick Mitchener <pmitchener@rogers.com>
2020-07-22 19:04:00 +0000

    Adding countLetter function

---
+++ countLetters.js
@@ -0,0 +1,21 @@
+const assertEqual = function(actual, expected) {
+  const passEmoji =  String.fromCodePoint(0x2705);
+  const failEmoji = '\u1F923';
+  actual === expected ? console.log(`${passEmoji} Assertion Passed: ${actual} === ${expected}`) : console.log(`${failEmoji} Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const countLetters = (sentence) => {
+  let obj = {}
+  for (char of sentence) {
+    if (char === " ") {
+      continue;
+    }
+    obj[char] ? obj[char]++ : obj[char] = 1;
+  }
+  return obj;
+};
+//console.log(countLetters("lighthouse in the house"));
+let result = countLetters("lighthouse in the house");
+assertEqual(result["t"], 2);
+assertEqual(result["e"], 3);
+assertEqual(result["o"], 2);
