c93d3205cd852aa4fc39f981331343bca83fc077
Patrick Mitchener <pmitchener@rogers.com>
2020-07-27 18:55:01 +0000

    Adding refractered tail function and test file for tail function.

--- tail.js
+++ tail.js
@@ -1,8 +1,3 @@
-const assertEqual = function(actual, expected) {
-  const passEmoji =  '';
-  const failEmoji = '';   
-  actual === expected ? console.log(`${passEmoji} Assertion Passed: ${actual} === ${expected}`) : console.log(`${failEmoji} Assertion Failed: ${actual} !== ${expected}`);
-};
 const tail = function(test) {
   if (!test) {
     return;
@@ -12,6 +7,4 @@ const tail = function(test) {
   }
   return test.slice(0);
 };
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+module.exports = tail;
