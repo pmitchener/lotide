50b5b969391133fb4268778ae9cf1ed2fe4b5e32
Patrick Mitchener <pmitchener@rogers.com>
2020-07-27 23:38:22 +0000

    Adding index object file with countLetters method.

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const head   = require('./head');
 const tail   = require('./tail');
 const middle = require('./middle');
 const eqObjects = require('./eqObjects');
+const countLetters = require('./countLetters');
 
 module.exports = {
   head:   head,
   tail:   tail,
   middle: middle,
-  eqObjects = eqObjects
+  eqObjects = eqObjects,
+  countLetters = countLetters
 };
