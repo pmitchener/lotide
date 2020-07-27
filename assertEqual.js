fc292a247aa65323452bef1caa39bff43bb689f4
Patrick Mitchener <pmitchener@rogers.com>
2020-07-27 18:41:55 +0000

    Cleanup assertEqual function and add inital test file for test cases in test folder

--- assertEqual.js
+++ assertEqual.js
@@ -1,10 +1,7 @@
 //could not get emojis to work.
 const assertEqual = function(actual, expected) {
-  const passEmoji =  '✅✅✅';
-  const failEmoji = '🛑🛑🛑';
+  const passEmoji =  '';
+  const failEmoji = '';  
   actual === expected ? console.log(`${passEmoji} Assertion Passed: ${actual} === ${expected}`) : console.log(`${failEmoji} Assertion Failed: ${actual} !== ${expected}`);
 };
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(3+4, 6);
-assertEqual(10+2 , 12);
+module.exports = assertEqual;
