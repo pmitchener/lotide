88a0aad37634cedc13ef88a6829387914f744f52
Patrick Mitchener <pmitchener@rogers.com>
2020-07-27 19:05:27 +0000

    Adding refractered assertArraysEqual function. Adding test file for assertArraysEqual function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,22 +1,7 @@
+const eqArrays = require("./eqArrays");
 const assertArraysEqual = function(list1, list2) {
   const passEmoji =  '';
   const failEmoji = '';
   eqArrays(list1, list2) ? console.log(`${passEmoji} Assertion Passed`) : console.log(`${failEmoji} Assertion Failed`);
 };
-
-const eqArrays = (list1, list2) => {
-  if(list1.length !== list2.length) {
-    return false;
-  }
-  for (let i = 0; i < list1.length; i++) {
-    if(list1[i] !== list2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // => should PASS
-assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
-
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => false
+module.exports = assertArraysEqual;
