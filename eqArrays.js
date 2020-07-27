f883ba7a90223f20dd9310de73e0550762eb79d2
Patrick Mitchener <pmitchener@rogers.com>
2020-07-27 19:00:14 +0000

    Adding refractered eqArrays function. Adding eqArrays test file.

--- eqArrays.js
+++ eqArrays.js
@@ -1,9 +1,3 @@
-const assertEqual = function(actual, expected) {
-  const passEmoji =  '';
-  const failEmoji = '';
-  actual === expected ? console.log(`${passEmoji} Assertion Passed: ${actual} === ${expected}`) : console.log(`${failEmoji} Assertion Failed: ${actual} !== ${expected}`);
-};
-
 const eqArrays = (list1, list2) => {
   if(list1.length !== list2.length) {
     return false;
@@ -15,8 +9,4 @@ const eqArrays = (list1, list2) => {
   }
   return true;
 };
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+module.exports = eqArrays;
