fbd38e77f83e3b714222ed5be5ee78fc9ddd5903
Patrick Mitchener <pmitchener@rogers.com>
2020-07-28 00:39:01 +0000

    Adding countOnly method to index object

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const tail   = require('./tail');
 const middle = require('./middle');
 const eqObjects = require('./eqObjects');
 const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
 
 module.exports = {
   head:   head,
   tail:   tail,
   middle: middle,
   eqObjects = eqObjects,
-  countLetters = countLetters
+  countLetters = countLetters,
+  countOnly = countOnly
 };
